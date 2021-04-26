643d035fe4ad8f5847cc1167054a4e4ae9b4d380
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-04-26 11:59:32 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,20 +5,19 @@ const Actors = () => {
   return (
     <div>
       <h1>Actors Page</h1>
-      <div>
-        {actors.map((actor) => {
-          return (
-            <div>
-              <h3>Name: {actor.name}</h3>
-              <ul>
-                {actor.movies.map((movie) => {
-                  return <li>{movie}</li>;
-                })}
-              </ul>
-            </div>
-          );
-        })}
-      </div>
+      {actors.map((actor) => {
+        return (
+          <div>
+            <h3>Name: {actor.name}</h3>
+            <p>Movies:</p>
+            <ul>
+              {actor.movies.map((movie) => {
+                return <li>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -6,21 +6,19 @@ const Directors = () => {
   return (
     <div>
       <h1>Directors Page</h1>
-      <div>
-        {directors.map((director) => {
-          return (
-            <div>
-              <h3>{`Name: ${director.name}`}</h3>
-              <p>Movies:</p>
-              <ul>
-                {director.movies.map((movie) => {
-                  return <li>{movie}</li>;
-                })}
-              </ul>
-            </div>
-          );
-        })}
-      </div>
+      {directors.map((director) => {
+        return (
+          <div>
+            <h3>{`Name: ${director.name}`}</h3>
+            <p>Movies:</p>
+            <ul>
+              {director.movies.map((movie) => {
+                return <li>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
